675bc081fed77f3c3fd0acc5a7bba7c7913bfacf
cturcu <ciprian.turcu@fortech.ro>
2015-06-25 08:36:19 -0400

    the basic code to aknowledge the plugin

---
+++ generate-thumbnails-advanced.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Generate Thumbnails - advanced
+Plugin URI: http://turcuciprian.com
+Description: A plugin that makes regenerating thumbnails even easier than before and more flexible.
+Version: 0.1
+Author: turcuciprian
+Author URI: http://turcuciprian.com
+License: GPLv2 or later
+Text Domain: akismet
+ */

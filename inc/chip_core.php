483dd726cb7a48b7d7cddb0133fc8c8ef831091d
cturcu <ciprian.turcu@fortech.ro>
2015-06-26 08:51:13 -0400

    added register settings commented a bunch of code

--- inc/chip_core.php
+++ inc/chip_core.php
@@ -1,7 +1,10 @@
 <?php
+
 global $cc_args;
+
 class chipcore {
-    
+
+//    create basic page in the admin panel, with menu settings too
     public function create_admin_page($args) {
         global $cc_args;
         $cc_args = $args;
@@ -11,13 +14,19 @@ class chipcore {
         add_action('admin_init', array($this, 'register_admin_page_callback'));
         return true;
     }
-    private function create_page_callback(){
+
+//    Callback for the admin_init hook - this is where the page is created.... text, form fields and all
+    private function create_page_callback() {
         global $cc_args;
-        
     }
-    private function register_admin_page_callback(){
+
+//    callback function for the add_menu_page - this is where the settings are registered
+    private function register_admin_page_callback() {
         global $cc_args;
-        
+        $args = $cc_args;
+        foreach ($args['fields'] as $field) {
+            register_setting($field['group'], $field['field_name']);
+        }
     }
 
 }

4b79f06d385b375abe12862ecf97880a4236f74f
cturcu <ciprian.turcu@fortech.ro>
2015-06-26 08:43:38 -0400

    created private callback functions passed global variable data for them

--- inc/chip_core.php
+++ inc/chip_core.php
@@ -1,13 +1,23 @@
 <?php
-
+global $cc_args;
 class chipcore {
-
-    function create_admin_page($args) {
+    
+    public function create_admin_page($args) {
+        global $cc_args;
+        $cc_args = $args;
         //create new top-level menu
         add_menu_page($args['page_title'], $args['menu_title'], $args['capability'], __FILE__, array($shis, 'create_page_callback'), plugins_url($args['icon_url'], __FILE__), $args['position']);
         //call register settings function
         add_action('admin_init', array($this, 'register_admin_page_callback'));
         return true;
+    }
+    private function create_page_callback(){
+        global $cc_args;
+        
+    }
+    private function register_admin_page_callback(){
+        global $cc_args;
+        
     }
 
 }

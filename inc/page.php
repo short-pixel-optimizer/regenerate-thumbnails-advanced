9a5746b28b666f4a7a0516381bffd6972fdbab93
cturcu <ciprian.turcu@fortech.ro>
2015-06-27 20:17:29 -0400

    created the menu item linking to the page

---
+++ inc/page.php
@@ -0,0 +1,17 @@
+<?php
+$args = array(
+    'page_title' => 'Generate Thumbnaiks Advanced',//the name of the page
+    'menu_title' => 'GT Adv',//the name of the menu
+    'capability' => 'administrator',//who has access to this
+    'menu_slug' => 'generate_thumbnails_advanced',//who has access to this
+    'icon_url' => '',//the icon of the menu
+    'position' => 81, //settings
+    'fields' => array(
+        array(
+            
+        )
+    )
+);
+/* var @cc cc */
+$cc = new cc();
+$cc->create_admin_page($args);

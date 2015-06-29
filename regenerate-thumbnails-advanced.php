fafbdcb597384eadf3ce741d3d310db3c54a53e0
cturcu <ciprian.turcu@fortech.ro>
2015-06-28 23:45:29 -0400

    renamed some functions and css classes added ajax start functionality started working on the details of ajax

--- regenerate-thumbnails-advanced.php
+++ regenerate-thumbnails-advanced.php
@@ -8,7 +8,7 @@
   Author: turcuciprian
   Author URI: http://turcuciprian.com
   License: GPLv2 or later
-  Text Domain: gta
+  Text Domain: RTA
  */
 
 //Global variables for arguments
@@ -21,10 +21,30 @@ class cc {
         add_action('admin_menu', array($this, 'create_menu'));
         add_action('admin_enqueue_scripts', array($this, 'enqueue_admin'));
         //ajax callback
-        add_action('wp_ajax_gta_rt',array($this,'ajax_callback'));
+        add_action('wp_ajax_rta_rt', array($this, 'ajax_callback'));
     }
-    public function ajax_callback(){
-        
+
+    public function ajax_callback() {
+        $offset = 0;
+        $args = array(
+            'post_type' => 'attachment',
+            'post_status' => 'any',
+            'posts_per_page' => 1,
+            'offset' => $offset
+        );
+        $the_query = new WP_Query($args);
+        if ($the_query->have_posts()) {
+            while ($the_query->have_posts()) {
+
+                $the_query->the_post();
+                echo $the_query->post->ID;
+            }
+        } else {
+            // no posts found
+        }
+        /* Restore original Post Data */
+        wp_reset_postdata();
+        wp_die();
     }
 
 //    Admin menu calback
@@ -32,7 +52,7 @@ class cc {
         global $cc_args;
         $args = $cc_args;
 //         Add a new submenu under Tools:
-        add_options_page(__('Generate Thumbnails Advanced', 'gta_id'), __('GT Adv', 'gta_id'), 'administrator', 'generate_thumbnails_advanced', array($this, 'create_page_callback'));
+        add_options_page(__('Generate Thumbnails Advanced', 'rta_id'), __('GT Adv', 'rta_id'), 'administrator', 'generate_thumbnails_advanced', array($this, 'create_page_callback'));
         //call register settings function
         add_action('admin_init', array($this, 'rapc'));
         return true;
@@ -45,15 +65,18 @@ class cc {
             }
         }
         wp_enqueue_script('jquery-ui-progressbar');
-        wp_enqueue_style('gta-jquery-ui', plugin_dir_url(__FILE__) . 'jquery-ui.css');
-        wp_enqueue_style('gta', plugin_dir_url(__FILE__) . 'style.css');
-        wp_enqueue_script('gta', plugin_dir_url(__FILE__) . 'script.js');
+        wp_enqueue_style('rta-jquery-ui', plugin_dir_url(__FILE__) . 'jquery-ui.css');
+        wp_enqueue_style('rta', plugin_dir_url(__FILE__) . 'style.css');
+        wp_enqueue_script('rta', plugin_dir_url(__FILE__) . 'script.js');
     }
 
 //    Callback for the admin_init hook - this is where the page is created.... text, form fields and all
     public function create_page_callback() {
+        $total = 1;
+        $offset = 0;
+
         $content .= sprintf('<!--GTA wrap START -->'
-                . '<div id="gta">');
+                . '<div id="rta">');
         $content .= sprintf("<h2>%s</h2>", 'Generate Thumbnails Advanced');
 //        Progress bar
         $content .= sprintf('<div id="progressbar">'
@@ -61,14 +84,16 @@ class cc {
                 . '</div>');
 //        Dropdown
         $content .= sprintf('<h3>Select a period</h3>');
-        $content .= sprintf('<select name="period" id="gta_period">');
+        $content .= sprintf('<select name="period" id="rta_period">');
         $content .= sprintf('<option value="0">All</option>');
         $content .= sprintf('<option value="1">past day</option>');
         $content .= sprintf('<option value="2">past week</option>');
         $content .= sprintf('<option value="2">past Month</option>');
         $content .= sprintf('</select>');
+        $content .= sprintf('<input type="hidden" name="total" value="%s"/>', $total);
+        $content .= sprintf('<input type="hidden" name="offset" value="%s"/>', $offset);
         $content .= sprintf('<p class="submit">'
-                . '<button class="button button-primary">Regenerate Thumbnails</button>'
+                . '<button class="button button-primary RTA">Regenerate Thumbnails</button>'
                 . '</p>');
 
         $content .= sprintf('</div>'
@@ -79,7 +104,7 @@ class cc {
 
 //    callback function for the add_menu_page - this is where the settings are registered
     public function rapc() {
-        
+        // 
     }
 
 }
--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 jQuery(document).ready(function ($) {
-    var pbar = $("#gta #progressbar");
+    var pbar = $("#rta #progressbar");
 //    if the progressbar id exists
     if (pbar[0]) {
 //        set the initial value to 0
         pbar.progressbar({
-            value: 0
+            value: 50
         });
-        //tha ajax data
-        var data = {
-            'action': 'gta_rt',
-            'whatever': 1234
-        };
+    }
+    var rta_butt = $('.button.RTA');
+    if (rta_butt[0]) {
+        rta_butt.click(function () {
+            //tha ajax data
+            var data = {
+                'action': 'rta_rt',
+                'whatever': 1234
+            };
 
-        // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
-        $.post(ajaxurl, data, function (response) {
-            alert('Got this from the server: ' + response);
+            // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+            $.post(ajaxurl, data, function (response) {
+                alert('Got this from the server: ' + response);
+            });
         });
     }
 });

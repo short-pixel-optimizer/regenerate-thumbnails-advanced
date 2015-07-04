59285ef16ff663bd3c4c1074dac63cc0212d06cb
cturcu <cturcu@FortechDev13_2.internal.mywebgrocer.com>
2015-07-04 11:14:14 -0400

    set the dropdown box as a object

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function ($) {
     var pbar = $("#rta #progressbar");
 //    When the page first loads
-    change_ajax_call();
+    first_load_ajax();
 //    if the progressbar id exists
     if (pbar[0]) {
 //        set the initial value to 0
@@ -25,16 +25,22 @@ jQuery(document).ready(function ($) {
             });
         }
         //ajax request to call to return the total and offset values ----- when some options are changed, when page is first loaded
-        function change_ajax_call() {
-            //tha ajax data
-            var data = {
-                'action': 'rta_ajax',
-                'whatever': 1234
-            };
-            // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
-            $.post(ajaxurl, data, function (response) {
-                alert('Got this from the server: ' + response);
-            });
+        function first_load_ajax() {
+//            set the dropdown box as a object variable
+            var rta_period = $('#rta_period');
+            //get the value from the dropdown box
+            var period = rta_period.val();
+            if (rta_period[0]) {
+                //tha ajax data
+                var data = {
+                    'action': 'rta_ajax',
+                    'type': 'general'
+                };
+                // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+                $.post(ajaxurl, data, function (response) {
+                    alert('Got this from the server: ' + response);
+                });
+            }
         }
     }
 });

77a91922d961cb22f7d78237233228b7843c3a90
cturcu <cturcu@FortechDev13_2.internal.mywebgrocer.com>
2015-07-04 22:09:56 -0400

    reorganized, improved and simplified javascript

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function ($) {
     var pbar = $("#rta #progressbar");
 //    When the page first loads
-    first_load_ajax();
+    loop_ajax_request('general', 0, -1, 0);
 //    if the progressbar id exists
     if (pbar[0]) {
 //        set the initial value to 0
@@ -23,53 +23,37 @@ jQuery(document).ready(function ($) {
             if (rta_total[0]) {
                 tCount = rta_total.html();
             }
-            loop_ajax_request(offset, tCount, period.val());
+            loop_ajax_request('submit', offset, tCount, period.val());
         }
         //
         //
         // Main ajax call
         //
         //
-        function loop_ajax_request(offset, tCount, period) {
+        function loop_ajax_request(type, offset, tCount, period) {
             //tha ajax data
             var data = {
                 'action': 'rta_ajax',
-                'type': 'submit',
+                'type': type,
                 'period': period,
                 'offset': offset
             };
             // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
             $.post(ajaxurl, data, function (response) {
-                alert('Got this from the server: ' + response);
+
+                switch (type) {
+                    case 'general':
+                        var rta_total = $('#rta .info .total');
+                        if (rta_total[0]) {
+                            var json = JSON.parse(response);
+                            rta_total.html(json.pCount);
+                        }
+                        break;
+                    case 'submit':
+                        alert('Got this from the server: ' + response);
+                        break;
+                }
             });
-        }
-        //
-        //FIRST LOAD ... ajax request to call to return the total and offset values ----- when some options are changed, when page is first loaded
-        //
-        function first_load_ajax() {
-//            set the dropdown box as a object variable
-            var rta_period = $('#rta_period');
-            //get the value from the dropdown box
-            var period = rta_period.val();
-            if (rta_period[0]) {
-                //tha ajax data
-                var data = {
-                    'action': 'rta_ajax',
-                    'type': 'general'
-                };
-                var rta_total = $('#rta .info .total');
-
-                // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
-                $.post(ajaxurl, data, function (response) {
-                    var json = JSON.parse(response);
-//                    Store the data as a cookie
-                    var rta_total = $('#rta .info .total');
-                    if (rta_total[0]) {
-                        rta_total.html(json.pCount);
-                    }
-                });
-
-            }
         }
     }
 });

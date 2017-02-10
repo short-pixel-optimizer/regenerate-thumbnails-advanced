9d616d986f5403048241d9332d7326ec3731638c
Ciprian Turcu <turcuciprian1@gmail.com>
2017-02-10 23:13:57 +0200

    implemented REST functionality with javascript and php

--- regenerate-thumbnails-advanced.php
+++ regenerate-thumbnails-advanced.php
@@ -3,11 +3,11 @@
   Plugin Name: reGenerate Thumbnails - advanced
   Plugin URI: http://turcuciprian.com
   Description: A plugin that makes regenerating thumbnails even easier than before and more flexible.
-  Version: 1.3.2.2
+  Version: 1.3.3
   Author: turcuciprian
   Author URI: http://turcuciprian.com
   License: GPLv2 or later
-  Text Domain: RTA
+  Text Domain: rta
  */
 
 //Global variables for arguments
@@ -25,191 +25,7 @@ class cc {
     }
 
     public function ajax_callback() {
-        if (isset($_POST['type'])) {
-            $type = $_POST['type'];
-        }
-        $logstatus ='';
-        $offset = 0;
-        switch ($type) {
-            case 'general':
-                $args = array(
-                    'post_type' => 'attachment',
-                    'posts_per_page' => -1,
-                    'post_status' => 'any',
-                    'offset' => 0
-                );
-
-                if (isset($_POST['period'])) {
-                    $period = $_POST['period'];
-
-                    switch ($period) {
-                        case '0':
-                            break;
-                        case '1':
-                            $date = '-1 day';
-                            break;
-                        case '2':
-                            $date = '-1 week';
-//                            echo $date;
-                            break;
-                        case '3':
-                            $date = '-1 month';
-                            break;
-                        case '4':
-                            $date = $_POST['fromTo'];
-                            break;
-                    }
-                }
-                if (isset($date) && !empty($date)) {
-                    $fromTo = explode('-', $date);
-                    $startDate = date("m/d/Y", strtotime($fromTo[0] . " -1 day"));
-                    $endDate = date("m/d/Y", strtotime($fromTo[1] . " +1 day"));
-
-
-
-                    if (!empty($startDate) && empty($endDate)) {
-                        $args['date_query'] = array('after' => $startDate);
-                    } elseif (!empty($endDate) && empty($startDate)) {
-                        $args['date_query'] = array('before' => $endDate);
-                    } elseif (!empty($startDate) && !empty($endDate)) {
-                        $args['date_query'] = array('after' => $startDate, 'before' => $endDate);
-                    }
-                }
-                $the_query = new WP_Query($args);
-                $post_count = 0;
-                if ($the_query->have_posts()) {
-                    $post_count = $the_query->post_count;
-                }
-                wp_reset_query();
-                wp_reset_postdata();
-                $logstatus .= "<pre>" . print_r($the_query, true) . "</pre>";
-                if(isset($_POST['type'])){
-                    $typeV = $_POST['type'];
-                }
-                if(!isset($date) || empty($date)){
-                    $date = '';
-                }
-                $return_arr = array('pCount' => $post_count, 'fromTo' => $date, 'type' => $typeV, 'period' => $period);
-//                return the total number of results
-
-
-                echo json_encode($return_arr);
-                break;
-            case 'submit':
-                $logstatus = '';
-                $error = array();
-                if (isset($_POST['offset'])) {
-                    $offset = $_POST['offset'];
-                }
-                if (isset($_POST['period'])) {
-                    $period = $_POST['period'];
-
-                    $args = array(
-                        'post_type' => 'attachment',
-                        'post_status' => 'any',
-                        'posts_per_page' => 1,
-                        'offset' => $offset,
-                        'orderby' => 'ID',
-                        'order' => 'DESC'
-                    );
-
-                    switch ($period) {
-                        case '0':
-                            break;
-                        case '1':
-                            $date = '-1 day';
-
-                            break;
-                        case '2':
-                            $date = '-1 week';
-                            break;
-                        case '3':
-                            $date = '-1 month';
-                            break;
-                        case '4':
-
-                            $date = $_POST['fromTo'];
-                            break;
-                    }
-                }
-
-                $args = array(
-                    'post_type' => 'attachment',
-                    'post_status' => 'any',
-                    'posts_per_page' => 1,
-                    'offset' => $offset,
-                );
-
-                if ($period != 0 && isset($date)) {
-
-                    if (!empty($date)) {
-                        $fromTo = explode('-', $date);
-                        $startDate = date("m/d/Y", strtotime($fromTo[0]));
-                        $endDate = date("m/d/Y", strtotime($fromTo[1] . " +1 day"));
-
-
-
-                        if (!empty($startDate) && empty($endDate)) {
-                            $args['date_query'] = array('after' => $startDate);
-                        } elseif (!empty($endDate) && empty($startDate)) {
-                            $args['date_query'] = array('before' => $endDate);
-                        } elseif (!empty($startDate) && !empty($endDate)) {
-                            $args['date_query'] = array('after' => $startDate, 'before' => $endDate);
-                        }
-                    }
-//                    $args = array_merge($args, $period_arr);
-                }
-                $the_query = new WP_Query($args);
-                if ($the_query->have_posts()) {
-                    while ($the_query->have_posts()) {
-
-                        $the_query->the_post();
-                        $image_id = $the_query->post->ID;
-                        $is_image = true;
-                        $fullsizepath = get_attached_file($image_id);
-                        //is image:
-                        if (!is_array(getimagesize($fullsizepath))) {
-                            $is_image = false;
-                        }
-                        if ($is_image) {
-                            if (false === $fullsizepath || !file_exists($fullsizepath))
-                                $error[] = '<code>' . esc_html($fullsizepath) . '</code>';
-
-                            @set_time_limit(900);
-                            $metadata = wp_generate_attachment_metadata($image_id, $fullsizepath);
-                            //get the attachment name
-                            $filename_only = basename(get_attached_file($image_id));
-                            if (is_wp_error($metadata)) {
-                                $error[] = sprint_f("%s Image ID:%d", $metadata->get_error_message(), $image_id);
-                            }
-                            if (empty($metadata)) {
-                                //$this->die_json_error_msg($image_id, __('Unknown failure reason.', 'regenerate-thumbnails'));
-                                $error[] = sprint_f('Unknown failure reason. regenerate-thumbnails %d', $image_id);
-                            } else {
-                                wp_update_attachment_metadata($image_id, $metadata);
-                            }
-                            $logstatus = "<br/>" . $filename_only . " - <b>Processed</b>";
-                        } else {
-                            $filename_only = basename(get_attached_file($image_id));
-
-                            $error[] = sprintf('Attachment (<b>%s</b> - ID:%d) is not an image. Skipping', $filename_only, $image_id);
-                        }
-                    }
-                } else {
-                    $error[] = "No pictures uploaded";
-                }
-                if (!extension_loaded('gd') && !function_exists('gd_info')) {
-                    $error[] = "<b>PHP GD library is not installed</b> on your web server. Please install in order to have the ability to resize and crop images";
-                }
-                //increment offset
-                $result = $offset + 1;
-                echo json_encode(array('offset' => ($offset + 1), 'error' => $error, 'logstatus' => $logstatus, 'startTime' => $_POST['startTime'], 'fromTo' => $_POST['fromTo'], 'type' => $_POST['type'], 'period' => $period));
-                break;
-        }
-        /* Restore original Post Data */
-        wp_reset_query();
-        wp_reset_postdata();
-        wp_die();
+      
     }
 
 //    Admin menu calback
@@ -232,6 +48,8 @@ class cc {
         wp_enqueue_style('rta-jquery-ui', plugin_dir_url(__FILE__) . 'jquery-ui.min.css');
         wp_enqueue_style('rta', plugin_dir_url(__FILE__) . 'style.css');
         wp_enqueue_script('rta', plugin_dir_url(__FILE__) . 'script.js');
+        //
+        wp_add_inline_script( 'jquery-migrate', 'var rtaRestURL = \''.site_url().'/wp-json/rta/regenerate\';' );
     }
 
 //    Callback for the admin_init hook - this is where the page is created.... text, form fields and all
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function($) {
     //no js error
     $('#no-js').addClass('hidden');
     $('#js-works').removeClass('hidden');
@@ -8,9 +8,9 @@ jQuery(document).ready(function ($) {
     var errors_obj = $('#rta .errors');
     var pbar = $("#rta #progressbar");
 
-//    if the progressbar id exists
+    //    if the progressbar id exists
     if (pbar[0]) {
-//        set the initial value to 0
+        //        set the initial value to 0
         pbar.progressbar({
             value: 0
         });
@@ -23,23 +23,27 @@ jQuery(document).ready(function ($) {
         var datepickerInputs = $('.datepicker');
         if (datepickerInputs[0]) {
             datepickerInputs.datepicker({
-                onSelect: function (valTo) {//min/max dates set
+                onSelect: function(valTo) { //min/max dates set
                     var dateStart = $('.datepicker.start');
                     var dateEnd = $('.datepicker.end');
                     if ($(this).hasClass('start')) {
-                        dateEnd.datepicker("change", {minDate: valTo});
+                        dateEnd.datepicker("change", {
+                            minDate: valTo
+                        });
                     } else {
-                        dateStart.datepicker("change", {maxDate: valTo});
+                        dateStart.datepicker("change", {
+                            maxDate: valTo
+                        });
                     }
                 }
             });
         }
-        period.change(function (value) {
+        period.change(function(value) {
             //if the date from-to option is selected
             if (parseInt($(this).val()) === 4) {
-                fromTo.removeClass('hidden');//show the fields
+                fromTo.removeClass('hidden'); //show the fields
             } else {
-                fromTo.addClass('hidden');//Hide fields / keep the fields hidden
+                fromTo.addClass('hidden'); //Hide fields / keep the fields hidden
             }
         });
     }
@@ -48,11 +52,10 @@ jQuery(document).ready(function ($) {
         var logstatus = $('#rta .logstatus');
         var errstatus = $('#rta .errors');
 
-        rta_butt.click(submit_ajax_call);
         //
         //LOOP REQUEST ... ajax request to call when the button is pressed
         //
-        function submit_ajax_call() {
+        var submit_ajax_call = function() {
             logstatus.html('Processing...');
             errstatus.html('Processing...');
             err_arr = [];
@@ -70,17 +73,17 @@ jQuery(document).ready(function ($) {
             loop_ajax_request('general', 0, -1, period.val(), startTime, fromTo);
 
 
-        }
+        };
+        rta_butt.click(submit_ajax_call);
+
         //
         //
         // Main ajax call
         //
         //
-        function loop_ajax_request(type, offset, tCount, period, startTime, fromTo) {
-
+        var loop_ajax_request = function(type, offset, tCount, period, startTime, fromTo) {
             //tha ajax data
             var data = {
-                'action': 'rta_ajax',
                 'type': type,
                 'startTime': startTime,
                 'period': period,
@@ -88,29 +91,24 @@ jQuery(document).ready(function ($) {
                 'fromTo': fromTo
             };
             // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
-            $.post(ajaxurl, data, function (response) {
-
+            $.post(rtaRestURL, data, function(response) {
                 //var err_arr = new Array();
                 //json response
-                console.log(response);
-                var json = JSON.parse(response);
-                
-//                console.log(json);
+                var json = response;
+
                 var startTime = json.startTime;
                 var offset = 0;
                 var tCount = 0;
-                
+
                 var rta_total = $('#rta .info .total');
                 var type = json.type;
-
                 switch (type) {
                     case 'general':
-                        
                         var period = $('#rta_period');
-                        var rta_total = $('#rta .info .total');
+                        rta_total = $('#rta .info .total');
                         var rta_processed = $('#rta .info .processed');
                         if (rta_total[0]) {
-                            var json = JSON.parse(response);
+                            json = response;
                             rta_total.html(json.pCount);
                             rta_processed.html("0");
                         }
@@ -118,8 +116,8 @@ jQuery(document).ready(function ($) {
                         if (rta_total[0]) {
                             tCount = rta_total.html();
                         }
-                        var startTime = new Date().getTime();
-                        var fromTo = json.fromTo;
+                        startTime = new Date().getTime();
+                        fromTo = json.fromTo;
                         loop_ajax_request('submit', offset, tCount, json.period, startTime, fromTo);
 
                         break;
@@ -130,7 +128,7 @@ jQuery(document).ready(function ($) {
                         var processed = $('#rta .info .processed');
 
                         var progressbar_percentage = $('#progressbar .progress-label');
-                        if (processed[0] && rta_total.html() != 0) {
+                        if (processed[0] && rta_total.html() !== 0) {
                             processed.html(json.offset);
                         }
                         tCount = parseInt(tCount);
@@ -139,9 +137,8 @@ jQuery(document).ready(function ($) {
                             offset = response;
                             var lPercentage = offset / tCount * 100;
                             if (pbar[0]) {
-                                if (progressbar_percentage[0]) {
-                                }
-//                                set the initial value to 0
+                                if (progressbar_percentage[0]) {}
+                                //                                set the initial value to 0
                                 pbar.progressbar({
                                     value: lPercentage
                                 });
@@ -160,19 +157,19 @@ jQuery(document).ready(function ($) {
                                 //append unique errors
                                 unique_arr_append(json.error);
                                 //make a new request to the ajax call
-                                var startTime = new Date().getTime();
+                                startTime = new Date().getTime();
                                 fromTo = json.fromTo;
                                 loop_ajax_request(type, offset, tCount, json.period, startTime, fromTo);
                             } else {
                                 console.log('Processing Completed!');
                                 var errStatus = $('#rta .errors');
-                                var logstatus = $('#rta .logstatus');
-                                logstatus.append('<br/>Completed !')
+                                logstatus = $('#rta .logstatus');
+                                logstatus.append('<br/>Completed !');
                                 if (errStatus.html() == "Processing...") {
                                     errStatus.html('No Errors.');
                                 }
                                 //the loop ended show errors and messages
-                                $.each(err_arr, function (index, value) {
+                                $.each(err_arr, function(index, value) {
                                     var final_val = '<div class="ui-state-error">' + value + '</div>';
                                     errors_obj.html(errors_obj.html() + final_val);
                                 });
@@ -181,17 +178,18 @@ jQuery(document).ready(function ($) {
                         } else {
                             unique_arr_append(json.error);
                             //the loop ended show errors and messages
-                            $.each(err_arr, function (index, value) {
+                            $.each(err_arr, function(index, value) {
                                 var final_val = '<div class="ui-state-error">' + value + '</div>';
                                 errors_obj.html(errors_obj.html() + final_val);
                             });
                         }
                         break;
                 }
+
             });
-        }
+        };
         // Append only unique array values
-        function unique_arr_append(val) {
+        var unique_arr_append = function(val) {
             var unique = true;
             var i = 0;
             var y = 0;
@@ -200,7 +198,7 @@ jQuery(document).ready(function ($) {
                 y = 0;
                 while (err_arr[y]) {
                     if (err_arr[y] == val[i]) {
-//                        console.log(err_arr[i]);
+                        //                        console.log(err_arr[i]);
                         unique = false;
                         break;
                     }
@@ -212,6 +210,6 @@ jQuery(document).ready(function ($) {
                 i++;
             }
 
-        }
+        };
     }
 });

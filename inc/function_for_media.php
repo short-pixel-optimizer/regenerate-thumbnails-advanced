c3c9b2a93d71e94d0f8ab059062f3feaf29ef623
ioanachi <ioana_chichi@outlook.com>
2017-09-03 21:12:23 +0300

    Last retouch

--- inc/function_for_media.php
+++ inc/function_for_media.php
@@ -1,55 +1,10 @@
  <?php
 
-add_action('admin_init', 'admin_init_media');
-
-function admin_init_media()
-{
-    $id = 47;
-    $get_path = get_attached_file($id);
-    // echo $get_path;
-    $file_name = take_name($get_path);
-    $file_name = explode(".", $file_name)[0];
-    $finalpath = getPath($get_path);
-    $images = scandir($finalpath);
-    foreach ($images as $key => $value) {
-        $a = strpos($value, $file_name.'-');
-        echo $a." ";
-        $filetodelete='';
-        if ($a===0) {
-            $filetodelete = $finalpath.'/'.$value;
-            // echo "<pre>";
-            // print_r($filetodelete);
-            unlink($filetodelete);
-        }
-    }
-
-    //
-    // echo "<pre>";
-    // print_r($images);
-    exit;
-}
-    // $query_images_args = array(
-    //     'post_type' => 'attachment',
-    //     'post_mime_type' => 'image',
-    //     'post_status' => 'inherit',
-    //     'posts_per_page' => - 1,
-    // );
-    // $query_images = new WP_Query($query_images_args);
-    // $images = array();
-    // foreach ($query_images->posts as $image) {
-    //     $images[] = $image->guid;
-    // }
-    // $array = fileName($images);
-    // echo "<pre>";
-    // print_r($array);
-
-
 function fileName($images)
 {
     $arra = [];
     foreach ($images as $key => $value) {
         $url = $value;
-        //$url = 'http://localhost/wordpress/wp-content/uploads/2017/09/file0002063905655.jpg';
 
         $arr = strrpos($url, '/');
         $arr2 = $arr + 1;
@@ -76,22 +31,23 @@ function getPath($path)
     return $partial_path;
 }
 
-function delete_thumbnails($id){
-
-  $get_path = get_attached_file($id);
-  // echo $get_path;
-  $file_name = take_name($get_path);
-  $file_name = explode(".", $file_name)[0];
-  $finalpath = getPath($get_path);
-  $images = scandir($finalpath);
-  foreach ($images as $key => $value) {
-      $a = strpos($value, $file_name.'-');
-      echo $a." ";
-      $filetodelete='';
-      if ($a===0) {
-          $filetodelete = $finalpath.'/'.$value;
-          // echo "<pre>";
-          // print_r($filetodelete);
-          unlink($filetodelete);
+function delete_thumbnails($id)
+{
+    $get_path = get_attached_file($id);
+    // echo $get_path;
+    $file_name = take_name($get_path);
+    $file_name = explode(".", $file_name)[0];
+    $finalpath = getPath($get_path);
+    $images = scandir($finalpath);
+    foreach ($images as $key => $value) {
+        $a = strpos($value, $file_name.'-');
+        echo $a." ";
+        $filetodelete='';
+        if ($a===0) {
+            $filetodelete = $finalpath.'/'.$value;
+            // echo "<pre>";
+            // print_r($filetodelete);
+            unlink($filetodelete);
+        }
+    }
 }
-}}

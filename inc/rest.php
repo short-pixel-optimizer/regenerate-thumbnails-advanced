d6616bae5f5db9220b27e30bdb8a16298cd73dd1
ciprianturcu <turcuciprian1@gmail.com>
2018-04-22 18:47:34 +0000

    added CORS function

--- inc/rest.php
+++ inc/rest.php
@@ -1,16 +1,18 @@
 <?php
-//
-// function rta_customize_rest_cors() {
-// 	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
-// 	add_filter( 'rest_pre_serve_request', function( $value ) {
-// 		header( 'Access-Control-Allow-Origin: '.get_home_url() );
-// 		header( 'Access-Control-Allow-Methods: GET,POST,PUSH' );
-// 		header( 'Access-Control-Allow-Credentials: true' );
-// 		header( 'Access-Control-Expose-Headers: Link', false );
-// 		return $value;
-// 	} );
-// }
-// add_action( 'rest_api_init', 'rta_customize_rest_cors', 15 );
+function rtaCors(){
+	 function rta_customize_rest_cors() {
+		remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+		add_filter( 'rest_pre_serve_request', function( $value ) {
+			header( 'Access-Control-Allow-Origin: '.get_home_url() );
+			header( 'Access-Control-Allow-Methods: GET,POST,PUSH' );
+			header( 'Access-Control-Allow-Credentials: true' );
+			header( 'Access-Control-Expose-Headers: Link', false );
+			return $value;
+		} );
+	 }
+	 add_action( 'rest_api_init', 'rta_customize_rest_cors', 15 );
+}
+//rtaCors();
 //
 $rtaRESTObj = new rtaREST();
 class rtaREST

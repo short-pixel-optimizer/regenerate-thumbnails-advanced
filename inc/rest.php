05ea20ec417146f04ed5be74b23edd74ce396d8a
turcuciprian <turcuciprian1@gmail.com>
2018-04-24 01:57:02 +0300

    cors comment

--- inc/rest.php
+++ inc/rest.php
@@ -1,16 +1,16 @@
 <?php
 //
-function my_customize_rest_cors() {
-	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
-	add_filter( 'rest_pre_serve_request', function( $value ) {
-		header( 'Access-Control-Allow-Origin: '.get_home_url() );
-		header( 'Access-Control-Allow-Methods: GET,POST,PUSH' );
-		header( 'Access-Control-Allow-Credentials: true' );
-		header( 'Access-Control-Expose-Headers: Link', false );
-		return $value;
-	} );
-}
-add_action( 'rest_api_init', 'my_customize_rest_cors', 15 );
+// function rta_customize_rest_cors() {
+// 	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+// 	add_filter( 'rest_pre_serve_request', function( $value ) {
+// 		header( 'Access-Control-Allow-Origin: '.get_home_url() );
+// 		header( 'Access-Control-Allow-Methods: GET,POST,PUSH' );
+// 		header( 'Access-Control-Allow-Credentials: true' );
+// 		header( 'Access-Control-Expose-Headers: Link', false );
+// 		return $value;
+// 	} );
+// }
+// add_action( 'rest_api_init', 'rta_customize_rest_cors', 15 );
 //
 $rtaRESTObj = new rtaREST();
 class rtaREST
